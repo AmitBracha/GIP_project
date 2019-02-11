6137e44b18e98c7932c81a275009787900f9cdee
AmitBracha <amitbracha0@gmail.com>
2019-02-11 12:00:15 +0200

    trying new model with only momentum_features instead of edge_feature

--- models/dgcnn.py
+++ models/dgcnn.py
@@ -44,9 +44,7 @@ def get_model(point_cloud, is_training, bn_decay=None):
     net = tf.reduce_max(net, axis=-2, keep_dims=True)
     net1 = net
 
-    adj_matrix = tf_util.pairwise_distance(net)
-    nn_idx = tf_util.knn(adj_matrix, k=k)
-    edge_feature = tf_util.get_edge_feature(net, nn_idx=nn_idx, k=k)
+    edge_feature = tf_util.get_momentum_feature(net, k=k)
 
     net = tf_util.conv2d(edge_feature, 64, [1, 1],
                          padding='VALID', stride=[1, 1],
@@ -55,9 +53,7 @@ def get_model(point_cloud, is_training, bn_decay=None):
     net = tf.reduce_max(net, axis=-2, keep_dims=True)
     net2 = net
 
-    adj_matrix = tf_util.pairwise_distance(net)
-    nn_idx = tf_util.knn(adj_matrix, k=k)
-    edge_feature = tf_util.get_edge_feature(net, nn_idx=nn_idx, k=k)
+    edge_feature = tf_util.get_momentum_feature(net, k=k)
 
     net = tf_util.conv2d(edge_feature, 64, [1, 1],
                          padding='VALID', stride=[1, 1],
