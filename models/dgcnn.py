6c1e46eb27901332eeb29541f4b6e8b109c6374e
AmitBracha <amitbracha0@gmail.com>
2019-02-12 19:22:41 +0200

    add one layer model

--- models/dgcnn.py
+++ models/dgcnn.py
@@ -21,6 +21,34 @@ def placeholder_inputs(batch_size, num_point):
 def get_model(point_cloud, is_training, bn_decay=None):
     """ Classification PointNet, input is BxNx3, output Bx40 """
     batch_size = point_cloud.get_shape()[0].value
+    num_point = point_cloud.get_shape()[1].value
+    end_points = {}
+    k = 20
+
+    adj_matrix = tf_util.pairwise_distance(point_cloud)
+    nn_idx = tf_util.knn(adj_matrix, k=k)
+    edge_feature = tf_util.new_get_edge_feature(point_cloud, nn_idx=nn_idx, k=k)
+
+    net = tf_util.conv2d(edge_feature, 1024, [1, 1],
+                         padding='VALID', stride=[1, 1],
+                         bn=True, is_training=is_training,
+                         scope='dgcnn1', bn_decay=bn_decay)
+    net = tf.reduce_max(net, axis=-2, keep_dims=True)
+
+    # MLP on global point cloud vector
+    net = tf.reshape(net, [batch_size, -1])
+    net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training,
+                                  scope='fc1', bn_decay=bn_decay)
+    net = tf_util.dropout(net, keep_prob=0.5, is_training=is_training,
+                          scope='dp1')
+    net = tf_util.fully_connected(net, 40, activation_fn=None, scope='fc3')
+
+    return net, end_points
+
+
+def get_model_two_layer(point_cloud, is_training, bn_decay=None):
+    """ Classification PointNet, input is BxNx3, output Bx40 """
+    batch_size = point_cloud.get_shape()[0].value
     # point_cloud_2_momentum = tf.concat(
     #     [tf.multiply(point_cloud, point_cloud), tf.multiply(tf.roll(point_cloud, shift=1, axis=-1), point_cloud)],
     #     axis=-1)
