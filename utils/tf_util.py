dcd3eb152c716c2918d2788f2fef6750389f8e76
AmitBracha <amitbracha0@gmail.com>
2019-02-15 01:30:52 +0200

    add a layer of momentum and neighbors

--- utils/tf_util.py
+++ utils/tf_util.py
@@ -820,6 +820,7 @@ def get_edge_feature(point_cloud, nn_idx, k=20):
     edge_feature = tf.concat([point_cloud_central, point_nei_minus_self], axis=-1)
     return edge_feature
 
+
 def get_random_edge_feature(point_cloud, layer_index, k=20):
     """Construct edge feature for each point
     Args:
@@ -837,16 +838,17 @@ def get_random_edge_feature(point_cloud, layer_index, k=20):
 
     point_cloud_central = point_cloud
 
-
     point_cloud_central = tf.expand_dims(point_cloud_central, axis=-2)
     point_cloud_central = tf.tile(point_cloud_central, [1, 1, k, 1])
 
-    point_cloud_central_shape =point_cloud_central.get_shape().as_list()
+    point_cloud_central_shape = point_cloud_central.get_shape().as_list()
     n = point_cloud_central_shape[1] * point_cloud_central_shape[2] * point_cloud_central_shape[3]
 
-    random_feature = tf.multiply(tf.math.sqrt(2/tf.math.pow(n,layer_index-1)), tf.random.normal(point_cloud_central.get_shape()))
+    random_feature = tf.multiply(tf.math.sqrt(2 / tf.math.pow(n, layer_index - 1)),
+                                 tf.random.normal(point_cloud_central.get_shape()))
     edge_feature = tf.concat([point_cloud_central, random_feature], axis=-1)
     return edge_feature
+
 
 def get_zeros_edge_feature(point_cloud, k=20):
     """Construct edge feature for each point
@@ -865,15 +867,15 @@ def get_zeros_edge_feature(point_cloud, k=20):
 
     point_cloud_central = point_cloud
 
-
     point_cloud_central = tf.expand_dims(point_cloud_central, axis=-2)
     point_cloud_central = tf.tile(point_cloud_central, [1, 1, k, 1])
 
-    point_cloud_central_shape =point_cloud_central.get_shape()
+    point_cloud_central_shape = point_cloud_central.get_shape()
 
     zeros_feature = tf.zeros(point_cloud_central_shape)
     edge_feature = tf.concat([point_cloud_central, zeros_feature], axis=-1)
     return edge_feature
+
 
 def get_feature_double(point_cloud, k=20):
     """Construct edge feature for each point
@@ -896,6 +898,7 @@ def get_feature_double(point_cloud, k=20):
 
     edge_feature = tf.concat([point_cloud_central, point_cloud_central], axis=-1)
     return edge_feature
+
 
 def get_momentum_feature(point_cloud, k=20):
     """Construct edge feature for each point
@@ -926,11 +929,55 @@ def get_momentum_feature(point_cloud, k=20):
     tmp_point_momentum = tf.multiply(point_cloud_central, rolled_point_cloud)
     point_cloud_momentum = tmp_point_momentum
     rolled_point_cloud = tf.roll(rolled_point_cloud, shift=1, axis=-1)
-    for i in range(k-1):
+    for i in range(k - 1):
         tmp_point_momentum = tf.multiply(point_cloud_central, rolled_point_cloud)
         point_cloud_momentum = tf.concat([point_cloud_momentum, tmp_point_momentum], axis=-2)
         rolled_point_cloud = tf.roll(rolled_point_cloud, shift=1, axis=-1)
 
     point_cloud_central = tf.tile(point_cloud_central, [1, 1, k, 1])
     momentum_feature = tf.concat([point_cloud_central, point_cloud_momentum], axis=-1)
+    return momentum_feature
+
+
+def get_edge_feature_and_momentum(point_cloud, nn_idx, k=20):
+    """Construct edge feature for each point
+    Args:
+      point_cloud: (batch_size, num_points, 1, num_dims)
+      nn_idx: (batch_size, num_points, k)
+      k: int
+
+    Returns:
+      edge features: (batch_size, num_points, k, num_dims)
+    """
+    og_batch_size = point_cloud.get_shape().as_list()[0]
+    point_cloud = tf.squeeze(point_cloud)
+    if og_batch_size == 1:
+        point_cloud = tf.expand_dims(point_cloud, 0)
+
+    point_cloud_central = point_cloud
+
+    point_cloud_shape = point_cloud.get_shape()
+    batch_size = point_cloud_shape[0].value
+    num_points = point_cloud_shape[1].value
+    num_dims = point_cloud_shape[2].value
+
+    idx_ = tf.range(batch_size) * num_points
+    idx_ = tf.reshape(idx_, [batch_size, 1, 1])
+
+    point_cloud_flat = tf.reshape(point_cloud, [-1, num_dims])
+    point_cloud_neighbors = tf.gather(point_cloud_flat, nn_idx + idx_)
+
+    point_cloud_central = tf.expand_dims(point_cloud_central, axis=-2)
+    rolled_point_cloud = point_cloud_central
+    tmp_point_momentum = tf.multiply(point_cloud_central, rolled_point_cloud)
+    point_cloud_momentum = tmp_point_momentum
+    rolled_point_cloud = tf.roll(rolled_point_cloud, shift=1, axis=-1)
+    for i in range(k - 1):
+        tmp_point_momentum = tf.multiply(point_cloud_central, rolled_point_cloud)
+        point_cloud_momentum = tf.concat([point_cloud_momentum, tmp_point_momentum], axis=-2)
+        rolled_point_cloud = tf.roll(rolled_point_cloud, shift=1, axis=-1)
+
+    point_cloud_central = tf.tile(point_cloud_central, [1, 1, k, 1])
+    momentum_feature = tf.concat(
+        [point_cloud_central, point_cloud_momentum, point_cloud_neighbors - point_cloud_central], axis=-1)
     return momentum_feature
